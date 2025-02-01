87be3b99e2ce882bcc1daa24c38bff10002209eb
eldepor <mracing@gmail.com>
2025-02-01 19:35:58 +0100

    Update EPG_script.sh

--- .github/workflows/EPG_script.sh
+++ .github/workflows/EPG_script.sh
@@ -1,44 +1,35 @@
 #!/bin/bash
 
 sed -i '/^ *$/d' epgs.txt
-#sed -i '/^ *$/d' canales.txt
 
 rm -f EPG_temp*
 
-	date_stamp=$(date +"%d/%m/%Y %R") 
-	echo '<?xml version="1.0" encoding="UTF-8"?>' > EPGTOTAL.xml 
-	echo "<tv generator-info-name=\"dobleM $date_stamp\" generator-info-url=\"t.me/EPG_dobleM\">" >> EPGTOTAL.xml 
+date_stamp=$(date +"%d/%m/%Y %R") 
+echo '<?xml version="1.0" encoding="UTF-8"?>' > EPGTOTAL.xml 
+echo "<tv generator-info-name=\"dobleM $date_stamp\" generator-info-url=\"t.me/EPG_dobleM\">" >> EPGTOTAL.xml 
 
- 	while IFS=, read -r epg
-	do
- 		extension="${epg##*.}"
-		if [ "$extension" = "gz" ]; then
-			echo "Descomprimiendo archivo: $epg"
-			gzip -d -c "$epg" > EPG_temp00.xml
-		else
-			echo "Procesando archivo: $epg"
-			cp "$epg" EPG_temp00.xml
-		fi
-  		cat EPG_temp00.xml >> EPG_temp.xml
-	
-	
-		awk '/<channel id="/,/<\/channel>/' EPG_temp.xml >> EPG_temp_channel.xml
-		awk '/<programme/,/<\/programme>/' EPG_temp.xml >> EPG_temp_programme.xml
-	
-	#sed -n '/<channel id="/,/<\/channel>/p' EPG_temp.xml | grep -E '<channel|<\/channel>' >> EPGTOTAL.xml
-	#sed -n '/<channel id=/,/<\/channel/p' EPG_temp.xml >> EPGTOTAL.xml 
-	#sed -n '/<programme/,/<\/programme/p' EPG_temp.xml >> EPGTOTAL.xml
-	
-	done < epgs.txt
-	
-	cat EPG_temp_channel.xml >> EPGTOTAL.xml
-	cat EPG_temp_programme.xml >> EPGTOTAL.xml
-
-	echo '</tv>' >> EPGTOTAL.xml
-
- 	#gzip -c EPGTOTAL.xml > EPGTOTAL.xml.gz
+while IFS=, read -r epg
+do
+	extension="${epg##*.}"
+	if [ "$extension" = "gz" ]; then
+		echo "Descomprimiendo archivo: $epg"
+		gzip -d -c "$epg" > EPG_temp00.xml
+	else
+		echo "Procesando archivo: $epg"
+		cp "$epg" EPG_temp00.xml
+	fi
+	cat EPG_temp00.xml > EPG_temp.xml
 
 
+	awk '/<channel id="/,/<\/channel>/' EPG_temp.xml >> EPG_temp_channel.xml
+	awk '/<programme/,/<\/programme>/' EPG_temp.xml >> EPG_temp_programme.xml
+
+
+done < epgs.txt
+
+cat EPG_temp_channel.xml >> EPGTOTAL.xml
+cat EPG_temp_programme.xml >> EPGTOTAL.xml
+
+echo '</tv>' >> EPGTOTAL.xml
 
 rm -f EPG_temp*
-#rm -f EPGTOTAL.xml
