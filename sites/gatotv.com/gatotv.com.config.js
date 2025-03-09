3c199a3383fcff808bb915cd556db78adcce3fb0
eldepor <mracing@gmail.com>
2025-03-09 18:55:17 +0100

    Update gatotv.com.config.js

--- sites/gatotv.com/gatotv.com.config.js
+++ sites/gatotv.com/gatotv.com.config.js
@@ -79,7 +79,7 @@ function parseStart($item, date) {
   const time = $item('td:nth-child(1) > div > time').attr('datetime')
 
   return DateTime.fromFormat(`${date.format('YYYY-MM-DD')} ${time}`, 'yyyy-MM-dd HH:mm', {
-    zone: 'UTC-5'
+    zone: 'UTC-7'
   }).setZone('Europe/Madrid')
 }
 
@@ -87,7 +87,7 @@ function parseStop($item, date) {
   const time = $item('td:nth-child(2) > div > time').attr('datetime')
 
   return DateTime.fromFormat(`${date.format('YYYY-MM-DD')} ${time}`, 'yyyy-MM-dd HH:mm', {
-    zone: 'UTC-5'
+    zone: 'UTC-7'
   }).setZone('Europe/Madrid')
 }
 
